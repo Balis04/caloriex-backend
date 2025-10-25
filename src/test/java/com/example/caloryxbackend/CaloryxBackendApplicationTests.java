a2a2f08973bda55ee656744a9ebbb8885125e575
Balis04 <balis.dominik.15@gmail.com>
2025-10-25 14:10:24 +0200

    Initial commit - Caloryx backend

---
+++ src/test/java/com/example/caloryxbackend/CaloryxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caloryxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloryxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
